d129bb3044fa0a39f60d2c5e330ef473cd7e1fe2
ragequitting101 <davidliang104@gmail.com>
2024-07-06 03:18:32 +0300

    Added schedule creator

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import urllib
 import matrix
 import api
 from tabulate import tabulate
+from openpyxl import Workbook
 
 def create_test_data_model():
     """Stores the data for the problem."""
@@ -42,43 +43,50 @@ def create_test_data_model():
     return data
 
 
-def create_data_model(depot, df, num_vehicles, vehicle_capacity):
-    """Stores the data for the problem."""
+def create_data_model(drivers_df, workers_df, vehicle_capacity):
+    '''Stores the data for the problem.'''
     data = {}
 
-    data["num_vehicles"] = num_vehicles
-    data["vehicle_capacities"] = [vehicle_capacity] * num_vehicles
-    data["depot"] = 0
-    data["addresses"] = [urllib.parse.quote(depot.strip())]
-    data["time_windows"] = [(0, 86400)]
-    data["demands"] = [0]
-    data["pickups_deliveries"] = []
+    data['addresses'] = []
+    data['num_vehicles'] = drivers_df.shape[0]
+    data['vehicle_capacities'] = [vehicle_capacity] * data['num_vehicles']
+    data['depot'] = 0
+    data['driver_names'] = drivers_df['Name'].tolist()
+    data['worker_names'] = ['None']
+    data['time_windows'] = [(0, 86400)]
+    data['demands'] = [0]
+    data['pickups_deliveries'] = []
+    data['time_matrix'] = []
 
-    for index, row in df.iterrows():
+    for index, row in workers_df.iterrows():
         # Home
-        home_address = urllib.parse.quote(row["Home"].strip())
-        data["addresses"].append(home_address)
-        data["demands"].append(1)
-        data["time_windows"].append((0, 86400))
+        data['worker_names'].append(row['Name'])
+        home_address = urllib.parse.quote(row['From'].strip())
+        data['addresses'].append(home_address)
+        data['demands'].append(1)
+        data['time_windows'].append((0, 86400))
 
         # Destination
-        dest_address = urllib.parse.quote(row["Destination"].strip())
-        data["addresses"].append(dest_address)
-        dest_time = row["Drop off"]
+        data['worker_names'].append(row['Name'])
+        dest_address = urllib.parse.quote(row['To'].strip())
+        data['addresses'].append(dest_address)
+        dest_time = row['Arrival Time']
         dest_time_sec = dest_time.hour * 3600 + dest_time.minute * 60 + dest_time.second
-        data["time_windows"].append((dest_time_sec-300, dest_time_sec))
-        data["demands"].append(0)
+        data['time_windows'].append((dest_time_sec-300, dest_time_sec))
+        data['demands'].append(0)
 
         data["pickups_deliveries"].append([index*2+1, index*2+2])
 
-    # Build distance matrix
-    # data["distance_matrix"] = matrix.build_distance_matrix(data)
-
     # Build time matrix
     data['API_key'] = api.get_api_key()
-    data["time_matrix"] = matrix.create_time_matrix(data, traffic = True)
-    # data["time_matrix"] = [[0, 0, 384, 790, 384], [0, 0, 384, 790, 384], [352, 352, 12, 794, 12], [1044, 1044, 908, 9, 908], [352, 352, 12, 794, 12]]
-    # print(data['addresses'])
+    # time_matrix = matrix.create_time_matrix(data, traffic = True)
+    time_matrix = [[3, 34, 879, 34, 3, 34], [136, 14, 880, 14, 135, 14], [963, 996, 11, 996, 962, 996], [135, 14, 880, 14, 135, 14], [3, 34, 879, 34, 3, 34], [136, 14, 878, 14, 135, 14]]
+
+    # Distance to and from depot is 0
+    data['time_matrix'].append([0]*(1+len(time_matrix)))
+    for row in time_matrix:
+        data['time_matrix'].append([0] + row)
+    
     return data
 
 
@@ -108,7 +116,33 @@ def convert_seconds_to_hhmmss(seconds):
     hours = seconds // 3600
     minutes = (seconds % 3600) // 60
     seconds = seconds % 60
-    return f"{hours:02}:{minutes:02}:{seconds:02}"
+    # Determine AM or PM
+    period = 'AM' if hours < 12 else 'PM'
+    # Adjust hours to 12-hour format
+    if hours == 0:
+        hours = 12
+    elif hours > 12:
+        hours -= 12
+
+    return f"{hours:02}:{minutes:02}:{seconds:02} {period}"
+
+
+def convert_seconds_to_hhmm(seconds):
+    # Calculate hours and minutes
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    # Determine AM or PM
+    period = 'AM' if hours < 12 else 'PM'
+    # Adjust hours to 12-hour format
+    if hours == 0:
+        hours = 12
+    elif hours > 12:
+        hours -= 12
+
+    # Format the time string
+    time_str = f"{hours:02}:{minutes:02} {period}"
+
+    return time_str
 
 
 def print_2d_matrix(matrix):
@@ -117,6 +151,7 @@ def print_2d_matrix(matrix):
     
     # Print the formatted matrix using tabulate
     print(tabulate(formatted_matrix, tablefmt="grid"))
+    print()
 
 
 def print_solution(data, manager, routing, solution):
@@ -171,9 +206,6 @@ def print_solution(data, manager, routing, solution):
 
 
 def solve(data):
-    # Instantiate the data problem.
-    #data = create_data_model()
-
     # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(
         len(data["time_matrix"]), data["num_vehicles"], data["depot"]
@@ -196,7 +228,7 @@ def solve(data):
     # Define cost of each arc to be the times taken to travel the arc
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    # Add Time Windows constraint.
+    # Add Time constraint.
     time = "Time"
     routing.AddDimension(
         transit_callback_index,
@@ -206,13 +238,17 @@ def solve(data):
         time,
     )
     time_dimension = routing.GetDimensionOrDie(time)
+    # set a large coefficient for the global span of the routes (i,e, maximum time of routes)
+    # to minimize time of longest route
+    # time_dimension.SetGlobalSpanCostCoefficient(100)
+
     # Add time window constraints for each location except depot.
     for location_idx, time_window in enumerate(data["time_windows"]):
         if location_idx == data["depot"]:
             continue
         index = manager.NodeToIndex(location_idx)
         time_dimension.CumulVar(index).SetRange(time_window[0], time_window[1])
-    # Add time window constraints for each vehicle start node.
+    # Add time window constraints for each vehicle start and end node.
     depot_idx = data["depot"]
     for vehicle_id in range(data["num_vehicles"]):
         index = routing.Start(vehicle_id)
@@ -242,7 +278,6 @@ def solve(data):
         True,  # start cumul to zero
         "Capacity",
     )
-    capacity_dimension = routing.GetDimensionOrDie("Capacity")
 
     # Define Transportation Requests.
     for request in data["pickups_deliveries"]:
@@ -269,20 +304,63 @@ def solve(data):
     # Print solution on console.
     if solution:
         print_solution(data, manager, routing, solution)
+        create_schedule(data, manager, routing, solution)
     else:
-        print("No solution")
+        print("\nNo solution\n")
+
+    return solution
+
+
+def create_schedule(data, manager, routing, solution):
+    """Store schedule into Excel sheet."""
+    wb = Workbook()
+    ws = wb.active
+    row_count = 1
+    time_dimension = routing.GetDimensionOrDie("Time")
+    for vehicle_id in range(data["num_vehicles"]):
+        ws.append([data['driver_names'][vehicle_id]])
+        print([data['driver_names'][vehicle_id]])
+        ws.merge_cells(f'A{row_count}:E{row_count}')
+        row_count += 1
+        ws.append(['Name', 'From', 'To', 'Pick-up Time', 'Arrival Time'])
+        row_count += 1
+        index = routing.Start(vehicle_id)
+        while not routing.IsEnd(index):
+            time_var = time_dimension.CumulVar(index)
+            index = solution.Value(routing.NextVar(index))
+            ind = manager.IndexToNode(index)
+            row = []
+            if ind % 2 == 1:
+                name = data['worker_names'][ind]
+                home_address = urllib.parse.unquote(data['addresses'][ind - 1])
+                dest_address = urllib.parse.unquote(data['addresses'][ind])
+                # pickup_time = f'{convert_seconds_to_hhmm(solution.Min(time_var))} to {convert_seconds_to_hhmm(solution.Max(time_var))}'
+                pickup_time = convert_seconds_to_hhmm((solution.Min(time_var)+solution.Max(time_var)) // 2)
+                arrival_time = convert_seconds_to_hhmm(data['time_windows'][ind + 1][1])
+                row = [ name,
+                        home_address, 
+                        dest_address, 
+                        pickup_time, 
+                        arrival_time]
+                ws.append(row)
+                print(row)
+                row_count += 1
+        ws.append([])
+        row_count += 1
+
+    wb.save('Schedule.xlsx')
 
 def main():
     """Entry point of the program."""
 
-    depot = "Boulevard Salmiya" # Driver home location
-    # Read the Excel file
-    df = pd.read_excel('Test_sheet.xlsx')
-    num_vehicles = 2
+    # Read the Excel files
+    drivers_df = pd.read_excel('Test_Drivers.xlsx')
+    workers_df = pd.read_excel('Test_Workers.xlsx')
     vehicle_capacity = 2
-    data = create_data_model(depot, df, num_vehicles, vehicle_capacity)
+    data = create_data_model(drivers_df, workers_df, vehicle_capacity)
     print(data)
     print_2d_matrix(data['time_matrix'])
+
     solve(data)
 
 if __name__ == "__main__":
